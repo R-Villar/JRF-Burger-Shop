be3da6290b9f41c4ac770bc851faf0fcdba94189
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-01 14:00:30 -0500

    initialData

---
+++ src/BurgersData.js
@@ -0,0 +1,67 @@
+const burger = [{
+  
+    id : 0,
+    name : "Tribute Burger",
+    restaurant : "Honest Burgers",
+    web : "www.honestburgers.co.uk",
+    descripcion : "A mouth-watering honest beef burger",
+    ingredients : [{
+        0 : "Beef",
+        1:"american cheese",
+        2:"burger sauce",
+        3:"french mustard",
+        4:"pickes",
+        5:"onion",
+        6:"lettuce",
+    }],
+    
+    id : 1,
+    name: "Pulled Mooshie",
+    restaurant : "Mooshies",
+    web : "www.veganburger.org",
+    descripcion : "Spicy vegan burger with jackfruit",
+    ingredients: [{
+        0:"jackfruit",
+        1:"coleslaw",
+        2:"gluten free bun",
+
+    }],
+
+    id : 2,
+    name :  "Krabby Patty",
+    restaurant : "Krusty Krab",
+    web : "https://twitter.com/SpongeBob",
+    descripcion : "The only people who don't like a Krabby Patty have never tasted one.",
+    ingredients : [{
+        0:"bun",
+        1:"patty",
+        2:"ketchup",
+        3:"mustard",
+        4:"pickles",
+        5:"onions",
+        6:"lettuce",
+        7:"cheese",
+        8:"tomato",
+        9:"bun",
+    }],    
+
+    id : 3,
+    name : "Crunchy Nacho Burger" ,
+    restaurant : "Max Burgers" ,
+    web : "https://www.max.se/maten/meny/burgare/crunchy-nacho-burger/",
+    descripcion : "The best combination of crunchiness and softness, all in one single burger",
+    ingredients : [{
+        0:"sesame bun",
+        1:"salsa",
+        2:"cheddar",
+        3:"nachos",
+        4:"beef",
+        5:"tomato",
+        6:"pickled onion",
+        7:"lettuce",
+        8:"jalapeño mayonnaise",
+    }]  
+
+
+
+}]
