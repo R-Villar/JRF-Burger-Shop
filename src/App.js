71f53cea1beb375080903169e70446a1488b2b9c
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-01 16:13:34 -0500

    updatedfile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Home from "./Components/Home"
 
-
 function App() {
+
+
 
 return (
     <div className="App">
       <Home />
+      <h2>hello</h2>
     </div>
   );
 }
