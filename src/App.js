5d766e3e9872a1522580498cb94c590397763cac
Remberto Villar <rembertovillar@gmail.com>
2022-08-01 21:08:19 -0400

    information displaying on page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import Home from "./Components/Home"
 
 function App() {
-
-
+  
 
 return (
     <div className="App">
       <Home />
-      <h2>hello</h2>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardContainer from "./Cardcontainter";
 
 
 function Home() {
-     
+
+    const [burgersObj, setBurgersObj] = useState([])
+
+    // fetch request
+    useEffect(() => {
+      fetch('http://localhost:3000/burgers')
+        .then(res => res.json())
+        .then(data => setBurgersObj(data))
+    }, [])
+    // console.log(burgersObj)
+
     return (
         <div>
-            <CardContainer />
+            <CardContainer burgersObj={burgersObj} />
         </div>
     )
 }
