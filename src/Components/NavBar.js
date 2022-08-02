70c52218678d9f0cf5220ca46ab0af545ee09e05
Remberto Villar <rembertovillar@gmail.com>
2022-08-02 12:48:21 -0400

    style changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,18 +1,19 @@
-import { NavLink } from "react-router-dom"
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function NavBar() {
     return (
-        <div>    
-            <NavLink to="/">JRF Burger Shop</NavLink>
-            
-            <NavLink to="/Customize">Make your own Burger</NavLink>
-            
-            <NavLink to="/">Home</NavLink>
-            
-            <NavLink to="/cart">Cart</NavLink>
-            
-        </div>
+        <Navbar bg="dark" variant="dark" sticky="top">
+            <Container>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">JRF Burger Shop</Nav.Link>  
+                    <Nav.Link href="/Customize">Make your own Burger</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/cart">Cart</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>        
     )
 }
 export default NavBar
