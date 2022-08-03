e61f6690a1ddc0b3a31c81a09273a35fbfe1b0fb
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-03 13:55:44 -0500

    update20

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,19 @@
-function Cart () {
+function Cart ({addCustSelection}) {
+    
+    const paid = (e) => {
+        e.preventDefault()
+        alert('On its wait, thank you for your purchase')
+    }
+    
     return (
-        <div>
-            Cart
+        <div>   
+            <form onSubmit={paid}>
+                <input placeholder="Name on the card"></input>
+                <input id="ccn" type="tel" inputMode="numeric" pattern="[0-9\s]{13,19}" autoComplete="cc-number" maxLength="19" placeholder="xxxx xxxx xxxx xxxx"></input>
+                <input type="month" placeholder="Exp. Date"></input>
+                <input placeholder="CCV"></input>
+                <input type="submit"></input>
+            </form>
         </div>
     )
 }
